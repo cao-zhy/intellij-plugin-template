a70e803a815707ce5f2f99cc3aa7cd0a355a3e8e
caozhy <czy211@hotmail.com>
2021-07-02 21:53:50 +0800

    feat: 注册模板组

---
+++ src/com/github/czy211/ij/template/TemplatesGroup.java
@@ -0,0 +1,9 @@
+package com.github.czy211.ij.template;
+
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+
+public class TemplatesGroup extends DefaultActionGroup {
+  public TemplatesGroup() {
+    super("Templates", true);
+  }
+}
