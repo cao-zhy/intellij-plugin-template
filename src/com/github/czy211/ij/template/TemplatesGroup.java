91e7c9385aa984ea4fca8528bfde6cf84ba02596
caozhy <czy211@hotmail.com>
2021-07-03 02:01:56 +0800

    refactor: 在 TemplatesGroup 类重写 AnAction 类的 update 方法，设置模板组的可见性

--- src/com/github/czy211/ij/template/TemplatesGroup.java
+++ src/com/github/czy211/ij/template/TemplatesGroup.java
@@ -1,9 +1,23 @@
 package com.github.czy211.ij.template;
 
+import com.intellij.ide.IdeView;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.project.Project;
 
 public class TemplatesGroup extends DefaultActionGroup {
   public TemplatesGroup() {
     super("Templates", true);
+  }
+
+  @Override
+  public void update(AnActionEvent e) {
+    Project project = e.getProject();
+    IdeView view = LangDataKeys.IDE_VIEW.getData(e.getDataContext());
+    boolean enabled = project != null && view != null && view.getDirectories().length != 0;
+    Presentation presentation = e.getPresentation();
+    presentation.setEnabledAndVisible(enabled);
   }
 }
