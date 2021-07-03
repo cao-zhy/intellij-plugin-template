07a5ecfb3090fab8d1f230f7b4e3aa443c4efd0b
caozhy <czy211@hotmail.com>
2021-07-04 01:51:33 +0800

    refactor: License 模板组的名称 "LICENSE" 重命名为 "Licenses"

--- src/com/github/czy211/ij/template/LicenseTemplateGroupDescriptorFactory.java
+++ src/com/github/czy211/ij/template/LicenseTemplateGroupDescriptorFactory.java
@@ -7,7 +7,7 @@ import com.intellij.ide.fileTemplates.FileTemplateGroupDescriptorFactory;
 public class LicenseTemplateGroupDescriptorFactory implements FileTemplateGroupDescriptorFactory {
   @Override
   public FileTemplateGroupDescriptor getFileTemplatesDescriptor() {
-    FileTemplateGroupDescriptor group = new FileTemplateGroupDescriptor("LICENSE", AllIcons.FileTypes.Text);
+    FileTemplateGroupDescriptor group = new FileTemplateGroupDescriptor("Licenses", AllIcons.FileTypes.Text);
     for (String name : Constant.LICENSE_TEMPLATES) {
       group.addTemplate(name + ".txt");
     }
