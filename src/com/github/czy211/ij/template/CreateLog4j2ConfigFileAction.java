be1c600087501956f292a9411cfa5f68716dcbb9
caozhy <czy211@hotmail.com>
2021-07-07 20:28:32 +0800

    fix: 修复 Log4j-config.xsd 文件没有格式化的问题

--- src/com/github/czy211/ij/template/CreateLog4j2ConfigFileAction.java
+++ src/com/github/czy211/ij/template/CreateLog4j2ConfigFileAction.java
@@ -7,8 +7,12 @@ import com.intellij.ide.fileTemplates.FileTemplate;
 import com.intellij.ide.fileTemplates.FileTemplateManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.WriteAction;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.codeStyle.CodeStyleManager;
 
 public class CreateLog4j2ConfigFileAction extends AnAction {
   public CreateLog4j2ConfigFileAction() {
@@ -21,15 +25,22 @@ public class CreateLog4j2ConfigFileAction extends AnAction {
     if (dir == null) {
       return;
     }
+    Project project = dir.getProject();
     FileTemplate template = FileTemplateManager.getDefaultInstance().getInternalTemplate("Log4j2 Config");
     try {
       if (dir.getVirtualFile().findChild("Log4j-config.xsd") == null) {
         FileTemplate xsdTemplate = FileTemplateManager.getDefaultInstance().findInternalTemplate("Log4j2 Config Xsd");
-        CreateFileFromTemplateAction.createFileFromTemplate("Log4j-config", xsdTemplate, dir, null, false);
+        // 首次在该文件夹创建时，Log4j-config.xsd 会使用设置中 XML 文件的 CodeStyle 进行格式化
+        PsiFile psiFile = CreateFileFromTemplateAction.createFileFromTemplate("Log4j-config", xsdTemplate, dir, null,
+          false);
+        // 再次格式化，才会使用 EditorConfig 配置
+        if (psiFile != null) {
+          WriteAction.compute(() -> CodeStyleManager.getInstance(project).reformat(psiFile));
+        }
       }
       CreateFileFromTemplateAction.createFileFromTemplate("log4j2", template, dir, null, true);
     } catch (Exception ex) {
-      Messages.showErrorDialog(e.getProject(), CreateElementActionBase.filterMessage(ex.getMessage()), "Error");
+      Messages.showErrorDialog(project, CreateElementActionBase.filterMessage(ex.getMessage()), "Error");
     }
   }
 }
