5296217094dc602144d0b96e0f735de2b9112230
caozhy <czy211@hotmail.com>
2021-07-07 22:55:45 +0800

    fix: 修复创建 Log4j-config.xsd 文件插件报错的问题
    格式化 Log4j2 Config Xsd 模板文件，并取消格式化文件。

--- src/com/github/czy211/ij/template/CreateLog4j2ConfigFileAction.java
+++ src/com/github/czy211/ij/template/CreateLog4j2ConfigFileAction.java
@@ -7,16 +7,15 @@ import com.intellij.ide.fileTemplates.FileTemplate;
 import com.intellij.ide.fileTemplates.FileTemplateManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.codeStyle.CodeStyleManager;
 
 public class CreateLog4j2ConfigFileAction extends AnAction {
   public CreateLog4j2ConfigFileAction() {
     super("Log4j2 Config", "Create New Log4j2 Config File", AllIcons.FileTypes.Xml);
+    FileTemplate template = FileTemplateManager.getDefaultInstance().getInternalTemplate("Log4j2 Config Xsd");
+    template.setReformatCode(false);
   }
 
   @Override
@@ -29,14 +28,8 @@ public class CreateLog4j2ConfigFileAction extends AnAction {
     FileTemplate template = FileTemplateManager.getDefaultInstance().getInternalTemplate("Log4j2 Config");
     try {
       if (dir.getVirtualFile().findChild("Log4j-config.xsd") == null) {
-        FileTemplate xsdTemplate = FileTemplateManager.getDefaultInstance().findInternalTemplate("Log4j2 Config Xsd");
-        // 首次在该文件夹创建时，Log4j-config.xsd 会使用设置中 XML 文件的 CodeStyle 进行格式化
-        PsiFile psiFile = CreateFileFromTemplateAction.createFileFromTemplate("Log4j-config", xsdTemplate, dir, null,
-          false);
-        // 再次格式化，才会使用 EditorConfig 配置
-        if (psiFile != null) {
-          WriteAction.compute(() -> CodeStyleManager.getInstance(project).reformat(psiFile));
-        }
+        FileTemplate xsdTemplate = FileTemplateManager.getDefaultInstance().getInternalTemplate("Log4j2 Config Xsd");
+        CreateFileFromTemplateAction.createFileFromTemplate("Log4j-config", xsdTemplate, dir, null, false);
       }
       CreateFileFromTemplateAction.createFileFromTemplate("log4j2", template, dir, null, true);
     } catch (Exception ex) {
